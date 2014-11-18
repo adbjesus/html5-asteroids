ac0a587b094e0eaa7fb36600189a38eac0ad907e
Guilherme Graca <ggracase@gmail.com>
2014-11-18 02:55:18 +0000

    Player Cenas

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 var stage, renderer, player;
 init();
 
+key = {
+  up: false,
+  left: false,
+  right: false
+}
 
 function init(){
   stage = new PIXI.Stage(0x000000);
@@ -12,6 +17,52 @@ function init(){
   player = new PIXI.Sprite(texture);
   player.scale = new PIXI.Point(0.7,0.7);
   stage.addChild(player);
+  player.position.x = 200;
+  player.position.y = 200;
+  player.anchor.x = 0.5;
+  player.anchor.y = 0.5;
+  player.speed = 1;
+  player.forward = function(){
+    var multiplier = {
+      x: Math.cos(this.rotation),
+      y: Math.sin(this.rotation)
+    }
+
+    this.position.x += this.speed * multiplier.x;
+    this.position.y += this.speed * multiplier.y;
+  }
+
+  window.addEventListener('keydown', function(event) {
+    switch (event.keyCode) {
+      case 37: // Left
+        key.left = true;
+        break;
+
+      case 38: // Up
+        key.up = true;
+        break;
+
+      case 39: // Right
+        key.right = true;
+        break;
+    }
+  }, false);
+
+  window.addEventListener('keyup', function(event) {
+    switch (event.keyCode) {
+      case 37: // Left
+        key.left = false;
+        break;
+
+      case 38: // Up
+        key.up = false;
+        break;
+
+      case 39: // Right
+        key.right = false;
+        break;
+    }
+  }, false);
 
   requestAnimFrame( animate );
 }
@@ -19,6 +70,12 @@ function init(){
 function animate() {
   requestAnimFrame( animate );
 
+  if(key.up)
+    player.forward();
+  if(key.right)
+    player.rotation += 0.1;
+  if(key.left)
+    player.rotation -= 0.1;
 
   renderer.render(stage);
 }
