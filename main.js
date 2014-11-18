38ad1405a061ccc82ba3a13ac8288f9036938a7d
Alexandre Jesus <adbjesus@gmail.com>
2014-11-18 02:34:00 +0000

    Main working

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var stage, renderer, player;
+var asteroids = [];
+var asteroid_text = PIXI.Texture.fromImage("img/asteroid_1.png");
 init();
-
 
 function init(){
 	stage = new PIXI.Stage(0x000000);
@@ -21,28 +22,37 @@ function init(){
 	requestAnimFrame( animate );
 }
 
+function spawn_asteroid(sx,sy){
+  idx = asteroids.length;
+  asteroid = new PIXI.Sprite(asteroid_text)
+  asteroid.anchor.x = 0.5;
+  asteroid.anchor.y = 0.5;
+  asteroid.position.x = Math.random()*renderer.width;
+  asteroid.position.y = Math.random()*renderer.height;
+  asteroid.scale.x = sx;
+  asteroid.scale.y = sy;
+  stage.addChild(asteroid);
+  asteroids.push(asteroid);
+}
+
 function animate() {
 	requestAnimFrame( animate );
 
-	function animate() {
-		requestAnimFrame( animate );
+	player.position.x += 5;
 
-		player.position.x += 5;
+	screenWrap(player);
 
-		screenWrap(player);
+	renderer.render(stage);
+}
 
-		renderer.render(stage);
-	}
-
-	function screenWrap(sprite) {
-		if (sprite.x > renderer.width){
-			sprite.x = 0;
-		}else if (sprite.x < 0){
-			sprite.x = renderer.width;
-		}else if (sprite.y > renderer.height){
-			sprite.y = 0;
-		}else if (sprite.y < 0){
-			sprite.y = renderer.height;
-		}
+function screenWrap(sprite) {
+	if (sprite.x > renderer.width){
+		sprite.x = 0;
+	}else if (sprite.x < 0){
+		sprite.x = renderer.width;
+	}else if (sprite.y > renderer.height){
+		sprite.y = 0;
+	}else if (sprite.y < 0){
+		sprite.y = renderer.height;
 	}
 }
