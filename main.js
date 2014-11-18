accb493e9ba080a1fcd8e0d314ba496fccb9e2e8
carlosf <8carlosf@gmail.com>
2014-11-18 02:16:34 +0000

    screenWrap

--- main.js
+++ main.js
@@ -10,6 +10,10 @@ function init(){
    //Player
    var texture = PIXI.Texture.fromImage("img/asteroid_1.png");
    player = new PIXI.Sprite(texture);
+	player.anchor.x = 0.5;
+	player.anchor.y = 0.5;
+	player.position.x = renderer.width/2;
+	player.position.y = renderer.height/2;
    stage.addChild(player);
 
    requestAnimFrame( animate );
@@ -21,7 +25,21 @@ function init(){
  function animate() {
      requestAnimFrame( animate );
 
-     player.position.x += 5;
+     player.position.y -= 5;
 
+
+   	screenWrap(player);
      renderer.render(stage);
  }
+
+function screenWrap(sprite) {
+	if (sprite.x > renderer.width){
+		sprite.x = 0;
+	}else if (sprite.x < 0){
+		sprite.x = renderer.width;
+	}else if (sprite.y > renderer.height){
+		sprite.y = 0;
+	}else if (sprite.y < 0){
+		sprite.y = renderer.height;
+	}
+}
