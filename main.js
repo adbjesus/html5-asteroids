4cf1d7c978eff8a939dbffa8d0477c4fb95855c0
Alexandre Jesus <adbjesus@gmail.com>
2014-11-18 02:05:07 +0000

    Good identation and new transparent ship

--- main.js
+++ main.js
@@ -3,25 +3,22 @@ init();
 
 
 function init(){
-   stage = new PIXI.Stage(0x000000);
-   renderer = PIXI.autoDetectRenderer(800, 600);
-   document.body.appendChild(renderer.view);
+  stage = new PIXI.Stage(0x000000);
+  renderer = PIXI.autoDetectRenderer(800, 600);
+  document.body.appendChild(renderer.view);
 
-   //Player
-   var texture = PIXI.Texture.fromImage("img/asteroid_1.png");
-   player = new PIXI.Sprite(texture);
-   stage.addChild(player);
+  //Player
+  var texture = PIXI.Texture.fromImage("img/ship.png");
+  player = new PIXI.Sprite(texture);
+  player.scale = new PIXI.Point(0.7,0.7);
+  stage.addChild(player);
 
-   requestAnimFrame( animate );
+  requestAnimFrame( animate );
 }
 
+function animate() {
+  requestAnimFrame( animate );
 
 
-
- function animate() {
-     requestAnimFrame( animate );
-
-     player.position.x += 5;
-
-     renderer.render(stage);
- }
+  renderer.render(stage);
+}
